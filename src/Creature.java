11988d5c5e6d083df3fa7666dbe4c6c3ca6d96f9
WillDeary <will@willdeary.co.uk>
2017-10-30 11:31:44 +0000

    Initial Commit, Created Creature class and Main Class

---
+++ src/Creature.java
@@ -0,0 +1,75 @@
+/**
+ * Created by k1764166 on 30/10/17.
+ */
+public class Creature {
+
+    int energy, fullness, happiness;
+    String name;
+
+    public Creature(String name)
+    {
+        energy = 15;
+        happiness = 15;
+        fullness = 15;
+
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void eat()
+    {
+        if (fullness >= 12) {
+            System.out.println("Whoops, "+ this.name +" is already full");
+        }else {
+            fullness += 8;
+            happiness -=6;
+            energy -=4;
+            if (fullness >15){fullness =15;}
+            System.out.println("Yummy " + this.name + "really enjoyed that meal");
+        }
+    }
+
+    public void play()
+    {
+        if (happiness >= 12) {
+            System.out.println("Whoops, "+ this.name +" is not tired");
+        }else {
+            happiness += 8;
+            fullness -=5;
+            energy -=5;
+            if (happiness >15){happiness =15;}
+            System.out.println("That was great fun!");
+
+        }
+    }
+
+    public void sleep()
+    {
+        energy += 8;
+        happiness -=6;
+        fullness -=4;
+        if (energy >15){energy =15;}
+        System.out.println("That was a good sleep!");
+
+    }
+    public String checkStatus()
+    {
+        String status = "";
+        if(happiness <=6){
+            status += "Oh"+ this.getName() + "is very sad \n";
+        }
+        if(energy <= 6)
+        {
+            status += "ZZZ"+ this.getName() + "is very tired! \n";
+
+        }
+        if(fullness<=6)
+        {
+            status += "Rumble"+ this.getName() + "is very hungry \n ";
+        }
+
+        return status;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Created by k1764166 on 30/10/17.
+ */
+public class Main {
+
+    public static void main(String args[])
+    {
+        System.out.println("Hello World");
+
+    }
+
+}
